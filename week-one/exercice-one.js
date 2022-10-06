b8c7b36e713f9de19650665a285026e211c363a2
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-10-06 18:24:10 -0500

    Ajustes a los archivos, se borro el del pruebas y se cambio el nombre del archivo defs a operations

--- week-one/exercice-one.js
+++ week-one/exercice-one.js
@@ -5,44 +5,43 @@
 // console.log(`${num1} + ${num2} = ${sum}`);
 
 const sum = (num1, num2) => {
-    let result = num1 + num2;
-    console.log(`${num1} + ${num2} = ${result}`);
+	let result = num1 + num2;
+	console.log(`${num1} + ${num2} = ${result}`);
+};
+
+function subtract(num1, num2) {
+	let result = num1 - num2;
+	console.log(`${num1} - ${num2} = ${result}`);
 }
 
+const multi = (num1, num2) => {
+	let result = num1 * num2;
+	console.log(`${num1} * ${num2} = ${result}`);
+};
 
-function subtract(num1, num2){
-    let result = num1 - num2;
-    console.log(`${num1} - ${num2} = ${result}`);
-}
+const dividir = (num1, num2) => {
+	let result = num1 / num2;
+	console.log(`${num1} / ${num2} = ${result}`);
+};
 
-const multi = (num1, num2)=>{
-    let result = num1 * num2;
-    console.log(`${num1} * ${num2} = ${result}`);
-}
+const potenciar = (num1, num2) => {
+	let result = num1 ** num2;
+	console.log(`${num1} ^ ${num2} = ${result}`);
+};
 
-const dividir = (num1, num2)=>{
-    let result = num1 / num2;
-    console.log(`${num1} / ${num2} = ${result}`);
-}
+const radicacion = (num1, num2) => {
+	console.log(`Raiz de ${num1} = ${Math.sqrt(num1)}`);
+	console.log(`Raiz de ${num2} = ${Math.sqrt(num2)}`);
+};
 
-const potenciar = (num1, num2)=>{
-    let result = num1 ** num2;
-    console.log(`${num1} ^ ${num2} = ${result}`);
-}
-
-const radicacion = (num1, num2)=>{
-    console.log(`Raiz de ${num1} = ${Math.sqrt(num1)}`);
-    console.log(`Raiz de ${num2} = ${Math.sqrt(num2)}`);
-}
-
-// exports.suma = sum; 
+// exports.suma = sum;
 // exports.resta = subtract;
 // exports.multipli = multi;
 // exports.divide = dividir;
 // exports.potencia = potenciar;
 // exports.raiz = radicacion;
 
-const operaciones = {}
+const operaciones = {};
 
 operaciones.suma = sum;
 operaciones.resta = subtract;
@@ -51,4 +50,4 @@ operaciones.divide = dividir;
 operaciones.potencia = potenciar;
 operaciones.raiz = radicacion;
 
-module.exports = operaciones
+module.exports = operaciones;
